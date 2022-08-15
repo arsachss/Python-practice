c51ef6eac09742a551975514ec973f28331e24d9
arsachss <104038412+arsachss@users.noreply.github.com>
2022-08-15 14:01:17 +0800

    Update qrcode_generator.py

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -2,6 +2,8 @@ import pyqrcode
 import png
 import cv2
 import os
+import glob
+from threading import Timer
 
 type_link = input("Enter your URL here.\n")
 file_name = input("Enter file name to be saved.\n")
@@ -12,10 +14,22 @@ qr_code.png(file_name +".png", scale=5)
 
 image = cv2.imread(file_name+'.png')
 #path to save the qrcode image
-path = r'D:\User Docs\Desktop\MyFolder'
+path = r'D:\User Docs\Desktop\New Folder' #<specify your directory to save the generated QrCode image as in example>
 (cv2.imwrite(os.path.join(path,file_name+'.png'), image))
 
-print("Done")
+#delete copied/duplicated qrcode
+def delete():
+    filename = file_name + ".png"
+    base_dir = "D:\\User Docs\\Desktop\\MyFolder\\python practice\\" #<specify this 'python file' saved path as in example>
+    fullpath = os.path.join(base_dir, filename)
+    os.remove(fullpath)
+    
+
+t = Timer(0.2, delete)
+t.start()
+
+print("Done created QrCode")
+
 
 
 
